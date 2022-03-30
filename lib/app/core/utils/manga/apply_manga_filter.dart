89329843f976cf74aa3c933c18d470ec6392cd4d
DattatreyaReddy <dattatreyareddypanta2001@gmail.com>
2022-03-30 08:38:28 +0530

    bug fixed: chapter and library Filters

--- lib/app/core/utils/manga/apply_manga_filter.dart
+++ lib/app/core/utils/manga/apply_manga_filter.dart
@@ -2,16 +2,17 @@ import '../../../data/enums/manga/manga_filter.dart';
 import '../../../data/manga_model.dart';
 
 bool applyMangaFilter(Map<MangaFilter, bool?> filter, Manga manga) {
+  var result = true;
   if (filter[MangaFilter.unread] != null) {
-    return !(filter[MangaFilter.unread]! ^ ((manga.unreadCount ?? 0) > 0));
+    result & (!(filter[MangaFilter.unread]! ^ ((manga.unreadCount ?? 0) > 0)));
   }
   if (filter[MangaFilter.downloaded] != null) {
-    return !(filter[MangaFilter.downloaded]! ^
-        ((manga.downloadCount ?? 0) > 0));
+    result &  (!(filter[MangaFilter.downloaded]! ^
+        ((manga.downloadCount ?? 0) > 0)));
   }
   if (filter[MangaFilter.completed] != null) {
-    return !(filter[MangaFilter.completed]! ^
-        ((manga.status ?? "") == "COMPLETED"));
+    result &  (!(filter[MangaFilter.completed]! ^
+        ((manga.status ?? "") == "COMPLETED")));
   }
-  return true;
+  return result;
 }
