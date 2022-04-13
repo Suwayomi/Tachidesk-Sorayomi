4173c542f54a95d43367e109da91fd2a4b99c450
DattatreyaReddy <dattatreyareddypanta2001@gmail.com>
2022-04-13 21:42:46 +0530

    Add App Update check

--- lib/app/core/values/string.dart
+++ lib/app/core/values/string.dart
@@ -1,2 +1,7 @@
-const String flutterGithubUrl =
+const String sorayomiGithubUrl =
     "https://github.com/Suwayomi/Tachidesk-Sorayomi";
+
+const String sorayomiLatestReleaseUrl =
+    "https://github.com/Suwayomi/Tachidesk-Sorayomi/releases/latest";
+const String sorayomiLatestReleaseApiUrl =
+    "https://api.github.com/repos/Suwayomi/Tachidesk-Sorayomi/releases/latest";
---
+++ lib/app/widgets/app_update_dialog.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../generated/locales.g.dart';
+import '../core/values/string.dart';
+
+Future<void> appUpdateDialog(String newRelease) {
+  return Get.defaultDialog(
+    title: LocaleKeys.appUpdate_newUpdateAvilable.tr,
+    content: Text(
+      LocaleKeys.appUpdate_versionAvilable.trParams(
+        {"version": newRelease},
+      ),
+    ),
+    confirm: ElevatedButton.icon(
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25),
+        ),
+      ),
+      onPressed: () async {
+        if (!await launch(sorayomiLatestReleaseUrl,
+            forceSafariVC: false, forceWebView: false)) {
+          Clipboard.setData(
+            ClipboardData(
+              text: sorayomiLatestReleaseUrl,
+            ),
+          );
+          Get.rawSnackbar(
+            title: LocaleKeys.error_launchURL_title.trParams({
+              "website": LocaleKeys.aboutScreen_gitHub.tr,
+            }),
+            message: LocaleKeys.error_launchURL_message.trParams(
+              {"url": sorayomiLatestReleaseUrl},
+            ),
+          );
+        }
+        Get.back();
+      },
+      icon: Icon(Icons.open_in_new),
+      label: Text(LocaleKeys.appUpdate_gitHub.tr),
+    ),
+    textCancel: LocaleKeys.appUpdate_close.tr,
+    onCancel: () {},
+  );
+}
